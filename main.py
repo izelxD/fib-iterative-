6ec6c3eb46474137291c152c288dbd415ebc4a45
izelxD <89963504+izelxD@users.noreply.github.com>
2022-11-16 15:52:50 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+COUNT = 0
+x=int(input("Enter Number of Terms :"))
+first=0
+sec=1
+c=0
+if(x<0):
+    print("Enter valid input..")
+elif(x==0):
+    print(0)
+elif(x==1):
+    print("Fibbonacci series upto",x,"is",first)
+else:
+    while c<x:
+        print(first) 
+        COUNT = COUNT +1
+        nth=first+sec 
+        COUNT = COUNT +1
+        first=sec 
+        sec=nth 
+        COUNT = COUNT +1
+        c+=1  
+        COUNT = COUNT +1
+        
+
+print("Steps required using Counter ", COUNT)
