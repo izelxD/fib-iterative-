ec1d35a94471934e172b921de948b68f87865428
izelxD <89963504+izelxD@users.noreply.github.com>
2022-11-16 22:03:38 +0530

    Create bank.sol

---
+++ bank.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.7.0; 
+//declared the solidity version
+
+//defining a contract
+contract MyContract {
+    address private owner;
+
+    //define a construstor
+
+    constructor(){
+        owner=msg.sender; 
+    }
+  
+    //function to get the address of the owner
+    function getOwner()
+    
+        public view returns (address) {
+            return owner;
+        }
+
+        //function to get return the balancr of the owner
+        function getbalance()
+        public view returns(uint256){
+            return owner.balance;
+        }
+
+        //function to deposit money
+        function deposit() external payable{
+            require(msg.value == 2 ether, "Please send two ethers");
+        }
+
+        //Function to withdraw money
+        function withdraw() external{
+            require(msg.sender == owner, "No");
+            msg.sender.transfer(address(this).balance);
+        }
+
+}
